cdf6407d1ea19d9af37b81fe718d82647b94ee97
fcaud <78268012+fcaud@users.noreply.github.com>
2021-07-03 16:27:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import "node_modules/bootstrap/dist/css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="row">
+          <div className="col">
+            <p>Hello world</p>
+          </div>
+          <div className="col">
+            <p>Hello world</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
