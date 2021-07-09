761bf0ba9e42c771dfa1ae54a8f0cda4acfbb6ba
fcaud <78268012+fcaud@users.noreply.github.com>
2021-07-09 17:17:54 +0100

    Added synonyms

--- src/Definitions.js
+++ src/Definitions.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Synonyms from "./Synonyms.js";
 
 export default function Definitions(props) {
   return (
@@ -8,9 +10,13 @@ export default function Definitions(props) {
         return (
           <div key={index}>
             <p>
-              {meaning.definition} <br />
-              <em>Example: {meaning.example}</em>
+              <strong>Definition:</strong> {meaning.definition} <br />
+              <em>
+                <strong>Example:</strong> {meaning.example}
+              </em>
             </p>
+            <Synonyms synonyms={meaning.synonyms} />
+            <br />
           </div>
         );
       })}
---
+++ src/Synonyms.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms row">
+        <div className="col">
+          <strong>Synonyms:</strong>
+        </div>
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <div className="col" key={index}>
+              {synonym}
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
