0e1c30b5584b0176e7e38507617b258a1d621cb4
fcaud <78268012+fcaud@users.noreply.github.com>
2021-07-10 14:42:24 +0100

    phonetics & play button

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Definitions from "./Definitions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function Results(props) {
   const results = props.results;
@@ -8,7 +10,20 @@ export default function Results(props) {
     return (
       <div>
         <h2>{results.word}</h2>
-        {results.meanings.map(function (meanings, index) {
+        {results.phonetics.map((phonetics, index) => {
+          const audioElement = new Audio(phonetics.audio);
+          return (
+            <div key={index}>
+              <button onClick={() => audioElement.play()}>
+                <FontAwesomeIcon icon={faVolumeUp} />
+              </button>
+              <p>
+                <em>{phonetics.text}</em>
+              </p>
+            </div>
+          );
+        })}
+        {results.meanings.map((meanings, index) => {
           return (
             <div key={index}>
               <Definitions definitions={meanings} />
